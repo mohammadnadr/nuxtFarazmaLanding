0fe08de9b722588acd6b6da071f2529ea7a96409
mohammad-nadr <amoo.nadr81@gmail.com>
2025-03-09 15:34:21 +0330

    add about

---
+++ pages/about.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <v-container>
+    <h1>Welcome to My Awesome App!</h1>
+    <p>This is the about page.</p>
+  </v-container>
+</template>
+
+<style scoped>
+
+</style>
